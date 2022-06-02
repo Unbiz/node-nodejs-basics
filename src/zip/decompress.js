737056105035a2357cf1b2f50f5ff911e230a603
Vladimir Bessonov <bessmy@gmail.com>
2022-06-02 13:57:36 +0400

    feat: implement zip functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,18 @@
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+
+const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
+const baseFilePath = path.resolve(moduleDirname, 'files/fileToCompress.txt');
+const compressFilePath = path.resolve(moduleDirname, 'files/archive.gz');
+
 export const decompress = async () => {
-    // Write your code here 
+    const gunzip = zlib.createGunzip();
+    const source = fs.createReadStream(compressFilePath);
+    const destination = fs.createWriteStream(baseFilePath);
+
+    source.pipe(gunzip).pipe(destination);
 };
+
+decompress();
