71eef527ed6280ad8041f8b2bf80f6a99acffb11
Vladimir Bessonov <bessmy@gmail.com>
2022-06-02 13:57:18 +0400

    feat: implement streams functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import fsProm from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.resolve(moduleDirname, 'files/fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+    const file = await fsProm.open(filePath);
+    const readable = file.createReadStream();
+    readable.pipe(process.stdout);
 };
+
+read();
