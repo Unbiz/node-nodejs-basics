071ff448aa0db706efbed4e56a40d9486796032a
Vladimir Bessonov <bessmy@gmail.com>
2022-06-02 13:55:51 +0400

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const argsStr = process.argv
+        .slice(2)
+        .map((item, i) => i % 2 === 0 ? `${item.slice(2)} is` : `${item},`)
+        .join(' ');
+
+    process.stdout.write(argsStr.slice(0, argsStr.length - 1));
 };
+
+parseArgs();
