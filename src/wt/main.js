66cc2a3ea6ba3946027d7dc582c67c03df2bc1ba
Vladimir Bessonov <bessmy@gmail.com>
2022-06-02 13:58:03 +0400

    feat: implement worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,42 @@
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
+const workerPath = path.resolve(moduleDirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+    function getWorker(workerData) {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, { workerData });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0) reject();
+            });
+        });
+    }
+
+    const workers = [];
+
+    for (let i = 10; i <= 13; i += 1) {
+        workers.push(getWorker(i));
+    }
+
+    const workersResults = await Promise.allSettled(workers).then((data) => {
+        const results = data.map((item) => {
+            if (item.status === 'fulfilled') {
+                return { status: 'resolved', value: item.value };
+            }
+
+            return { status: 'error', value: null };
+        });
+
+        return results;
+    });
+
+    console.log(workersResults);
+    return workersResults;
 };
+
+performCalculations();
