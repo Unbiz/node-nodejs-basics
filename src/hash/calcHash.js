9677cc7f1738fd979728fed157f42a25f40c7013
Vladimir Bessonov <bessmy@gmail.com>
2022-06-02 13:56:48 +0400

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+import fsProm from 'fs/promises';
+import path from 'path';
+
+const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
+const baseFilePath = path.resolve(moduleDirname, 'files/fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const fileText = await fsProm.readFile(baseFilePath, 'utf8');
+    const hash = crypto.createHash('sha256').update(fileText).digest('hex');
+    console.log(`Hash of ${baseFilePath} is ${hash}`);
+    return hash;
 };
+
+calculateHash();
