9dc9330c6b99f42afc4c5d975956e73e233f8773
Vladimir Bessonov <bessmy@gmail.com>
2022-06-02 13:58:42 +0400

    feat: implement child processes functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
+const childFilePath = path.resolve(moduleDirname, 'files/script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(childFilePath, args);
+    process.stdout.write('Please, input text\n(for quit press: "CTRL+C" or print: "CLOSE"):\n\n');
 };
+
+spawnChildProcess([1, 2, 3]);
