07a8cf10165095bfa27fc04cdcfe6105640949d9
Vladimir Bessonov <bessmy@gmail.com>
2022-06-05 22:56:17 +0400

    fix: add new line in cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,7 +4,7 @@ export const parseArgs = () => {
         .map((item, i) => i % 2 === 0 ? `${item.slice(2)} is` : `${item},`)
         .join(' ');
 
-    process.stdout.write(argsStr.slice(0, argsStr.length - 1));
+    process.stdout.write(argsStr.slice(0, argsStr.length - 1) + '\n');
 };
 
 parseArgs();
