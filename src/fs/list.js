d9717935bcbded954b487083ddccdfb9bc6abe4b
Vladimir Bessonov <bessmy@gmail.com>
2022-06-05 13:41:25 +0400

    feat: primn list of files in array

--- src/fs/list.js
+++ src/fs/list.js
@@ -5,25 +5,13 @@ import path from 'path';
 const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
 const baseFolderPath = path.resolve(moduleDirname, 'files');
 
-async function printFileInfo(file) {
-    const filePath = path.join(baseFolderPath, file);
-    const stat = await fsProm.stat(filePath);
-
-    if (!stat.isDirectory()) {
-        const ext = path.extname(file);
-        const name = path.basename(file, ext);
-        const info = `${name}${ext}`;
-        console.log(info);
-    }
-}
-
 export const list = async () => {
     try {
-        const files = await fsProm.readdir(baseFolderPath);
+        const files = (await fsProm.readdir(baseFolderPath, { withFileTypes: true }))
+            .filter(file => !file.isDirectory())
+            .map(file => file.name);
 
-        for (const file of files) {
-            printFileInfo(file);
-        }
+        console.log(files);
     } catch {
         throw new Error('FS operation failed');
     }
