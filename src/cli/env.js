8c0600b8c6fbf223072f8e53297a1861c4571d0b
Vladimir Bessonov <bessmy@gmail.com>
2022-06-05 22:58:17 +0400

    fix: add new line in cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,7 +2,7 @@ export const parseEnv = () => {
     const envVarPrefix = 'RSS_';
     const rssVarsArr = Object.entries(process.env).filter((varEl) => varEl[0].startsWith(envVarPrefix));
     const rssVarsStr = rssVarsArr.map((varEl) => `${varEl[0]}=${varEl[1]}`).join('; ');
-    process.stdout.write(rssVarsStr);
+    process.stdout.write(rssVarsStr + '\n');
 };
 
 parseEnv();
