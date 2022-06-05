c3621be60b151db20960ed6d74d498270c1dda42
Vladimir Bessonov <bessmy@gmail.com>
2022-06-05 17:19:27 +0400

    feat: get arguments from command line

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -4,10 +4,11 @@ import path from 'path';
 
 const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
 const childFilePath = path.resolve(moduleDirname, 'files/script.js');
+const argsArr = process.argv.slice(2);
 
 export const spawnChildProcess = async (args) => {
     const child = fork(childFilePath, args);
     process.stdout.write('Please, input text\n(for quit press: "CTRL+C" or print: "CLOSE"):\n\n');
 };
 
-spawnChildProcess([1, 2, 3]);
+spawnChildProcess(argsArr);
